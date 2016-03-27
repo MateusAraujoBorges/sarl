80c9b4c2e5083afb06c0bd8544908bc9673707c9
yihaoyan <yihaoyan@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2016-03-27 21:26:32 +0000

    Translation.java dealing with div and modulo in SARL
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2159 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- src/edu/udel/cis/vsl/sarl/prove/cvc/Translation.java
+++ src/edu/udel/cis/vsl/sarl/prove/cvc/Translation.java
@@ -4,15 +4,28 @@ import java.util.List;
 
 import edu.udel.cis.vsl.sarl.util.FastList;
 
+/**
+ * 
+ * This class is used to deal with div or modulo during the cvc translation
+ * Since cvc4 currently can not deal with div or modulo, div or modulo is 
+ * translated into the form : value && constraints. value is the actual value of
+ * the division or modulo operation (e.g., the value of the division is the quotient).
+ * The format of the constraints can be read below.
+ * 
+ * 
+ * @author yanyihao
+ * 
+ */
 public class Translation {
 	/**
 	 * store the translation result
 	 */
 	private FastList<String> result;
+	
 	/**
 	 * is the translation coming from division or module?
 	 */
-	private Boolean isDivOrModule;
+	private Boolean isDivOrModulo;
 	
 	/**
 	 * if the translation comes from division or module,
@@ -43,14 +56,14 @@ public class Translation {
 	
 	public Translation(){
 		result = new FastList<String>();
-		isDivOrModule = false;
+		isDivOrModulo = false;
 		auxConstraints = null;
 		auxVars = null;
 	}
 	
 	public Translation(FastList<String> res){
 		result = res;
-		isDivOrModule = false;
+		isDivOrModulo = false;
 		auxConstraints = null;
 		auxVars = null;
 	}
@@ -58,7 +71,7 @@ public class Translation {
 	public Translation(FastList<String> res, Boolean isDvOrMo, 
 			FastList<String> auxConstraints, List<FastList<String>> auxVars){
 		result = res;
-		isDivOrModule = isDvOrMo;
+		isDivOrModulo = isDvOrMo;
 		this.auxConstraints = auxConstraints;
 		this.auxVars = auxVars;
 	}
@@ -69,14 +82,14 @@ public class Translation {
 
 
 
-	public Boolean getIsDivOrModule() {
-		return isDivOrModule;
+	public Boolean getIsDivOrModulo() {
+		return isDivOrModulo;
 	}
 
 
 
-	public void setIsDivOrModule(Boolean isDivOrModule) {
-		this.isDivOrModule = isDivOrModule;
+	public void setIsDivOrModulo(Boolean isDivOrModule) {
+		this.isDivOrModulo = isDivOrModule;
 	}
 
 
@@ -107,7 +120,7 @@ public class Translation {
 		FastList<String> constraints = this.auxConstraints == null ? null :
 				this.auxConstraints.clone();
 		Translation translation = new Translation(this.result.clone(),
-				this.isDivOrModule, 
+				this.isDivOrModulo, 
 				constraints, 
 				this.auxVars);
 		return translation;
