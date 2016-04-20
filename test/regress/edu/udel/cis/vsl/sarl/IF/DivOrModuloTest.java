8f7f675a66232c47169528cbd2c3cf0fae8736ee
yihaoyan <yihaoyan@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2016-04-20 01:18:08 +0000

    minor changes
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2225 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- test/regress/edu/udel/cis/vsl/sarl/IF/DivOrModuloTest.java
+++ test/regress/edu/udel/cis/vsl/sarl/IF/DivOrModuloTest.java
@@ -16,7 +16,10 @@ public class DivOrModuloTest {
 	private SymbolicType integerType;
 	private NumericExpression x; 
 	private NumericExpression y;
+	private NumericExpression z;
 	private NumericExpression two;
+	private NumericExpression zero;
+	private NumericExpression one;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -27,7 +30,11 @@ public class DivOrModuloTest {
 				.symbolicConstant(universe.stringObject("x"), integerType);
 		y = (NumericExpression) universe
 				.symbolicConstant(universe.stringObject("y"), integerType);
+		z = (NumericExpression) universe
+				.symbolicConstant(universe.stringObject("z"), integerType);
 		two = universe.integer(2);
+		zero = universe.integer(0);
+		one = universe.integer(1);
 	}
 	
 	/**
@@ -51,20 +58,64 @@ public class DivOrModuloTest {
 	}
 	
 	/**
+	 * TODO: SARL solves it, cvc4 and z3 can not solve it
+	 * assumption:
+	 * x^2 + y = 1
+	 * x^2 - y = 1
 	 * 
+	 * predicate:
+	 * y = 0
+	 */
+	@Test
+	public void divisionTest2(){
+		BooleanExpression x2py = universe.equals(one, 
+				universe.add(y, 
+						universe.multiply(x, x)));
+		BooleanExpression x2my = universe.equals(one, 
+				universe.subtract(universe.multiply(x, x), 
+						y));
+		BooleanExpression assumption = universe.and(x2py, x2my);
+		BooleanExpression predicate = universe.equals(y, zero);
+		Reasoner r = universe.reasoner(assumption);
+		
+		ValidityResult result = r.valid(predicate);
+		assertEquals(ResultType.YES, result.getResultType());
+	}
+	
+	/**
+	 * assumption: x/y=z
+	 * predicate: x!=z
+	 * 
+	 * expected result: no
+	 */
+	@Test
+	public void divisionTest3(){
+		BooleanExpression assumption = universe.and(universe.equals(z, 
+				universe.divide(x, y)), 
+				universe.neq(y, zero));
+		BooleanExpression predicate = universe.neq(x, z);
+		Reasoner r = universe.reasoner(assumption);
+		
+		ValidityResult result = r.valid(predicate);
+		assertEquals(ResultType.NO, result.getResultType());
+	}
+	
+	/**
 	 * assumption: x%y = 2
 	 * predicate: x != y
 	 * 
-	 * result true;
+	 * expected result true;
 	 */
 	@Test
 	public void moduloTest1(){
-		BooleanExpression assumption = universe.equals(two, 
-				universe.modulo(x, y));
+		BooleanExpression assumption = universe.and(universe.equals(two, 
+				universe.modulo(x, y)),
+				universe.neq(y, zero));
 		BooleanExpression predicate = universe.neq(x, y);
 		Reasoner r = universe.reasoner(assumption);
 		
 		ValidityResult result = r.valid(predicate);
 		assertEquals(ResultType.YES, result.getResultType());
 	}
+	
 }
