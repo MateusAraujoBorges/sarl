09f9704252125a8546a6c8467ea5dd57024bc94c
sili <sili@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2016-06-24 14:38:30 +0000

    remove printExprTree() in tests
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2284 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- test/regress/edu/udel/cis/vsl/sarl/IF/PrintCompressedTest.java
+++ test/regress/edu/udel/cis/vsl/sarl/IF/PrintCompressedTest.java
@@ -46,8 +46,6 @@ public class PrintCompressedTest {
 				universe.multiply(e1, e1));
 
 		out.println("expr is: " + e2 + "\n");
-		out.println("====== original tree =======");
-		universe.printExprTree(e2, out);
 		out.println("====== compressed tree ======");
 		universe.printCompressed(e2, out);
 		out.println("====================");
@@ -62,8 +60,6 @@ public class PrintCompressedTest {
 		NumericExpression e2 = universe.add(x, e1);
 
 		out.println("expr is: " + e2 + "\n");
-		out.println("====== original tree =======");
-		universe.printExprTree(e2, out);
 		out.println("====== compressed tree ======");
 		universe.printCompressed(e2, out);
 		out.println("====================");
@@ -162,8 +158,6 @@ public class PrintCompressedTest {
 				universe.subtract(x, y));
 
 		out.println("expr is: " + e1 + "\n");
-		out.println("====== original tree =======");
-		universe.printExprTree(e1, out);
 		out.println("====== compressed tree ======");
 		universe.printCompressed(e1, out);
 		out.println("====================");
@@ -249,8 +243,6 @@ public class PrintCompressedTest {
 				.apply(r2rConst, Arrays.asList(e1)));
 
 		out.println("expr is: " + e4 + "\n");
-		out.println("====== original tree =======");
-		universe.printExprTree(e4, out);
 		out.println("====== compressed tree ======");
 		universe.printCompressed(e4, out);
 		out.println("====================");
@@ -271,5 +263,8 @@ public class PrintCompressedTest {
 				Arrays.asList(e1));
 
 		out.println("expr is: " + e4 + "\n");
+		out.println("====== compressed tree ======");
+		universe.printCompressed(e4, out);
+		out.println("====================");
 	}
 }
