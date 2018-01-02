8fa4dceef71df330a50785b7c7d0b69c7b0fdc5b
ziqing <ziqing@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2018-01-02 16:01:00 +0000

    Add a test. See ticket #839
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2614 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- test/regress/edu/udel/cis/vsl/sarl/simplify/SimplifyExpressionTest.java
+++ test/regress/edu/udel/cis/vsl/sarl/simplify/SimplifyExpressionTest.java
@@ -2,12 +2,15 @@ package edu.udel.cis.vsl.sarl.simplify;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import edu.udel.cis.vsl.sarl.SARL;
 import edu.udel.cis.vsl.sarl.IF.Reasoner;
 import edu.udel.cis.vsl.sarl.IF.SymbolicUniverse;
 import edu.udel.cis.vsl.sarl.IF.expr.BooleanExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.NumericSymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicConstant;
 import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
 import edu.udel.cis.vsl.sarl.IF.type.SymbolicType;
@@ -20,10 +23,10 @@ public class SimplifyExpressionTest {
 	@Test
 	public void conditionalExpr() {
 		// given X?Y:Y, it should be simplified to be Y
-		SymbolicConstant X = universe.symbolicConstant(
-				universe.stringObject("X"), boolType);
-		SymbolicConstant Y = universe.symbolicConstant(
-				universe.stringObject("Y"), intType);
+		SymbolicConstant X = universe
+				.symbolicConstant(universe.stringObject("X"), boolType);
+		SymbolicConstant Y = universe
+				.symbolicConstant(universe.stringObject("Y"), intType);
 		SymbolicExpression cond = universe.cond((BooleanExpression) X, Y, Y);
 		Reasoner reasoner = universe.reasoner(universe.trueExpression());
 		SymbolicExpression symplified = reasoner.simplify(cond);
@@ -31,5 +34,28 @@ public class SimplifyExpressionTest {
 		System.out.println("original expression: " + cond);
 		System.out.println("symplified expression: " + symplified);
 		assertTrue(universe.equals(Y, symplified).isTrue());
+	}
+
+	@Test
+	public void simplifyOpenRange() {
+		// When simplify following expression:
+		// (x + 1 <= 0) || (x + 2 <= 0) || (0 <= x - 2) || (0 <= x - 1)
+		// an error will happen.
+		NumericSymbolicConstant x = (NumericSymbolicConstant) universe
+				.symbolicConstant(universe.stringObject("X"),
+						universe.integerType());
+		BooleanExpression predicate, clause0, clause1, clause2, clause3;
+
+		clause0 = universe.lessThanEquals(x, universe.minus(universe.oneInt()));
+
+		clause1 = universe.lessThanEquals(x,
+				universe.minus(universe.integer(2)));
+		clause2 = universe.lessThanEquals(universe.oneInt(), x);
+		clause3 = universe.lessThanEquals(universe.integer(2), x);
+		predicate = universe
+				.or(Arrays.asList(clause0, clause1, clause2, clause3));
+		predicate = universe.reasoner(universe.trueExpression())
+				.simplify(predicate);
+		System.out.println(predicate);
 	}
 }
