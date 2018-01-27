56d06a2dbfeb0908cf2260d55fb095a2283a003e
ziqing <ziqing@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2018-01-27 03:40:30 +0000

    fixed a bug in IsomorphicComparator
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2657 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- src/edu/udel/cis/vsl/sarl/ideal/simplify/IsomorphicTreeComparator.java
+++ src/edu/udel/cis/vsl/sarl/ideal/simplify/IsomorphicTreeComparator.java
@@ -1,10 +1,13 @@
 package edu.udel.cis.vsl.sarl.ideal.simplify;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 import edu.udel.cis.vsl.sarl.ideal.simplify.FastEvaluator3.EvalNode;
 
 public class IsomorphicTreeComparator {
+
+	private static EvalNodeHashComparator comparator = new EvalNodeHashComparator();
 
 	public static boolean isIsomophicTrees(EvalNode tree0, EvalNode tree1) {
 		if (tree0.operator() != tree1.operator())
@@ -16,26 +19,33 @@ public class IsomorphicTreeComparator {
 		if (tree0.numChildren() == 0)
 			return true;
 
-		int hashCodes0[] = new int[tree0.numChildren()];
-		int hashCodes1[] = new int[hashCodes0.length];
-		EvalNode children0[] = tree0.getChildren();
-		EvalNode children1[] = tree1.getChildren();
+		int numChildren = tree0.numChildren();
+		EvalNode children0Copy[] = Arrays.copyOf(tree0.getChildren(),
+				numChildren);
+		EvalNode children1Copy[] = Arrays.copyOf(tree1.getChildren(),
+				numChildren);
 
-		for (int i = 0; i < hashCodes0.length; i++) {
-			hashCodes0[i] = children0[i].hashCode();
-			hashCodes1[i] = children1[i].hashCode();
-		}
-		Arrays.sort(hashCodes0);
-		Arrays.sort(hashCodes1);
+		Arrays.sort(children0Copy, comparator);
+		Arrays.sort(children1Copy, comparator);
 
-		boolean currLevelEq = Arrays.equals(hashCodes0, hashCodes1);
+		boolean currLevelEq = Arrays.equals(children0Copy, children1Copy);
 
 		if (!currLevelEq)
 			return false;
 		else
-			for (int i = 0; i < children0.length; i++)
-				if (!isIsomophicTrees(children0[i], children1[i]))
+			for (int i = 0; i < numChildren; i++)
+				if (!isIsomophicTrees(children0Copy[i], children0Copy[i]))
 					return false;
 		return true;
+	}
+
+	private static class EvalNodeHashComparator
+			implements Comparator<EvalNode> {
+
+		@Override
+		public int compare(EvalNode o1, EvalNode o2) {
+			return Integer.compare(o1.hashCode(), o2.hashCode());
+		}
+
 	}
 }
