7ca14a815bb6e82ab8539e786ce96cc903a99509
ziqing <ziqing@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2018-03-27 21:16:59 +0000

    add a under construction class for simplify non-concrete tuples
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2687 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

---
+++ src/edu/udel/cis/vsl/sarl/ideal/simplify/SymbolicTupleSimplifier.java
@@ -0,0 +1,52 @@
+package edu.udel.cis.vsl.sarl.ideal.simplify;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import edu.udel.cis.vsl.sarl.IF.expr.ReferenceExpression;
+import edu.udel.cis.vsl.sarl.IF.expr.SymbolicExpression;
+import edu.udel.cis.vsl.sarl.preuniverse.IF.PreUniverse;
+import edu.udel.cis.vsl.sarl.util.Pair;
+
+public class SymbolicTupleSimplifier {
+
+	/**
+	 * A reference to {@link #PreUniverse}
+	 */
+	private PreUniverse universe;
+
+	/**
+	 * A queue of equations that have not been processed. To process each
+	 * equation x == v,
+	 * <ul>
+	 * <li>if x is a TUPLE_READ operation, x == v will be processed and the
+	 * {@link #tupleComponents} map will be updated correspondingly, then remove
+	 * the equation.</li>
+	 * <li>if v is zero and x has the form <code>R.0 + (-1)*Y</code>, where R is
+	 * a {@link ReferenceExpression}, the {@link #tupleSubstitutions} map will
+	 * be updated by adding an entry <code><Y, R></code>, then remove the
+	 * equation.</li>
+	 * <li>otherwise, ignore and remove equation x == v.</li>
+	 * </ul>
+	 */
+	private LinkedList<Pair<SymbolicExpression, SymbolicExpression>> workingEquations;
+
+	/**
+	 * A map from symbolic expressions that have tuple types but whose operators
+	 * are not TUPLE to symbolic expressions whose operators are TUPLE. In other
+	 * words, this is a map from non-concrete tuples to concrete tuples.
+	 */
+	private Map<SymbolicExpression, SymbolicExpression> tupleSubstitutions;
+
+	/**
+	 * 
+	 */
+	private Map<SymbolicExpression, SymbolicExpression[]> tupleComponents;
+
+	SymbolicTupleSimplifier(Context context) {
+		this.workingEquations = new LinkedList<>();
+		this.tupleSubstitutions = new HashMap<>();
+		this.universe = context.info.universe;
+	}
+}
