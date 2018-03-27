84f704e92ab4334e29680986f26004f238b40951
ziqing <ziqing@7d651925-2d04-6a0e-8c93-cd1e20bcaab1>
2018-03-27 21:20:48 +0000

    update doc, still under construction
    git-svn-id: svn://vsl.cis.udel.edu/sarl/trunk@2688 7d651925-2d04-6a0e-8c93-cd1e20bcaab1

--- src/edu/udel/cis/vsl/sarl/ideal/simplify/SymbolicTupleSimplifier.java
+++ src/edu/udel/cis/vsl/sarl/ideal/simplify/SymbolicTupleSimplifier.java
@@ -18,7 +18,7 @@ public class SymbolicTupleSimplifier {
 
 	/**
 	 * A queue of equations that have not been processed. To process each
-	 * equation x == v,
+	 * equation x == v, where v is a concrete value,
 	 * <ul>
 	 * <li>if x is a TUPLE_READ operation, x == v will be processed and the
 	 * {@link #tupleComponents} map will be updated correspondingly, then remove
@@ -34,13 +34,18 @@ public class SymbolicTupleSimplifier {
 
 	/**
 	 * A map from symbolic expressions that have tuple types but whose operators
-	 * are not TUPLE to symbolic expressions whose operators are TUPLE. In other
-	 * words, this is a map from non-concrete tuples to concrete tuples.
+	 * are not TUPLE to symbolic expressions whose operators are TUPLE and
+	 * components have concrete values. In other words, this is a map from
+	 * non-concrete tuples to concrete tuples.
 	 */
 	private Map<SymbolicExpression, SymbolicExpression> tupleSubstitutions;
 
 	/**
-	 * 
+	 * A map from symbolic expressions that have tuple types but whose operators
+	 * are not TUPLE (i.e. non-concrete tuples) to their component values. For
+	 * each non-concrete tuple, if all of its component values can be simplified
+	 * to some concrete values, the non-concrete tuple can be simplified to a
+	 * concrete tuple.
 	 */
 	private Map<SymbolicExpression, SymbolicExpression[]> tupleComponents;
 
